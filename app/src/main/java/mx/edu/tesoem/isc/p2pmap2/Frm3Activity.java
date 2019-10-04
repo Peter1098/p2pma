fa24717b98d5d4593d6f62d9c8631ef55ce3d41d
Pedro Meza Alarcon <PeterMA1098>
2019-10-04 11:20:33 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p2pmap2/Frm3Activity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p2pmap2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Frm3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm3);
+    }
+}
